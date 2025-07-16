5f58f69a4b50e5ab47043e115854ed5a84eba973
protozioc <156637794+protozoic@users.noreply.github.com>
2025-07-16 12:14:37 -0400

    Added .cache to gitignore. Changed name of src/functions.mpp to src/functions.ixx

--- xmake.lua
+++ xmake.lua
@@ -5,10 +5,10 @@ add_requires("gtest", {configs = {main = true}})
 
 target("app")
     set_kind("binary")
-    add_files("src/*.cpp", "src/*.mpp")
+    add_files("src/*.cpp", "src/*.ixx")
 
 target("tests")
     set_kind("binary")
-    add_files("src/*.mpp", "tests/*.cpp")
+    add_files("src/*.ixx", "tests/*.cpp")
     add_packages("gtest")
     set_group("test")
